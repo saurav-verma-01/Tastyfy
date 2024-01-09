5112c53fc1c7505c92943aec2d05bf53a3fcd764
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-09 17:09:51 +0530

    Cors issue fix for all devices

--- src/constants.js
+++ src/constants.js
@@ -6665,7 +6665,7 @@ const resData = {
   csrfToken: "QKKuc9mVlCv2-NczLru1QmwDbYw2TkHzwDgGgKyU",
 };
 
-const SWIGGY_API_URL = `https://crossorigin.me/https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.4924134&lng=77.673673&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+const SWIGGY_API_URL = `https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D27.4924134%26lng%3D77.673673%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING`;
 
 const IMAGE_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
