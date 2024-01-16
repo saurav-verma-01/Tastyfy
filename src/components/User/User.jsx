4cc821f7b077fbf13dd9aaffabc52a86ac51cb58
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-01-16 23:28:24 +0530

    Learning to use State in class components

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import "./User.css";
 
 const User = ({ name, location }) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(2);
   return (
     <div className="user-fn">
       <h1>Name: {name}</h1>
       <h2>This is My Swiggy Clone Website Function</h2>
       <h3>Location: {location}</h3>
       <h3>Contact: Saurav345@gmail.com</h3>
+      <h2>Count - {count}</h2>
+      <h2>Count 2 - {count2}</h2>
     </div>
   );
 };
--- src/components/User/UserClass.jsx
+++ src/components/User/UserClass.jsx
@@ -5,17 +5,51 @@ class UserClass extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      countClass: 0,
+      countClass2: 22,
+    };
+
     console.log(props);
   }
 
   render() {
     const { name, location } = this.props;
+    const { countClass, countClass2 } = this.state;
     return (
       <div className="user-cls">
         <h1>Name: {name}</h1>
         <h2>This is My Swiggy Clone Website Class</h2>
         <h3>Location: {location}</h3>
         <h3>Contact: Saurav3class@gmail.com</h3>
+        <div className="set">
+          <h2>Count - {countClass}</h2>
+          <div className="btn-set">
+            <button
+              className="btn-sub"
+              onClick={() => {
+                this.setState({
+                  countClass: this.state.countClass - 1,
+                  countClass2: this.state.countClass2 - 2,
+                });
+              }}
+            >
+              -
+            </button>
+            <button
+              className="btn-add"
+              onClick={() => {
+                this.setState({
+                  countClass: this.state.countClass + 1,
+                  countClass2: this.state.countClass2 + 2,
+                });
+              }}
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <h2>Count - {countClass2}</h2>
       </div>
     );
   }
